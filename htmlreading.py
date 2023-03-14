9add77f729f578673f50324a0e07ef0b90080aeb
reddylopes <reddy.lopes@gmail.com>
2023-03-14 14:53:35 -0300

    Second version of the software
    From this version and beyond, Old and New Testament are supported.
    
    - The code has changed dramatically, because now the logic is to work the file verse by verse, instead of looking for the data independently
    - Added test (line 27) to see if the file has the expected structure of a custom exegetical guide.
    - Each verse has a <div> and a <blockquote>, which are searched after the test
    - All references (book, chapter and verse) and verse texts are searched inside the <div> and stored
    - Inside the blockquotes, the structure generated by Logos is: a <p> and three <blockquote>
    - From the <p><span>, we get the word in the original language
    - From the first <blockquote>, we get the lemma and the possible translations suggested by Logos
    - From the second <blockquote>, we get the morfological descriptions appending all <span> text inside the <p>
    - The scrapping in the blockquotes is made inside a while loop. This is because in hebrew, a "word" may be composed of several other words. Each of them is described by three <blockquote>;
    - Everything is inserted in a multidimensional list
    
    A .xlsx file is filled with the verse refs, the verse texts and the full table by xlsxwriter

--- htmlreading.py
+++ htmlreading.py
@@ -1,105 +1,130 @@
+# Lib para ler HTML
 from bs4 import BeautifulSoup
+
+# Lib para time.sleep(1) no debug
 import time
 
+# Lib para dialog
 import tkinter as tk
 from tkinter import filedialog
 
+# Lib para encerrar programa
+import sys
+
+# Abrir arquivo HTML
 root = tk.Tk()
 root.withdraw()
-
 file_path = filedialog.askopenfilename()
 #file_path = "C:\\Users\\jonat\\Desktop\\jo7.html"
 
 f = open(file_path, encoding="utf8")
+
+# Parse do HTML com BeautifulSoup
 html_doc = f.read()
-
 soup = BeautifulSoup(html_doc, 'html.parser')
+body = soup.find("body")
 
-####################################
-# Pegar todas as palavras do verso #
-####################################
-verse_words=[]
+# Teste: se o HTML não estiver no formato padrão, encerrra
+if len(body.find_all("h2")) > 1:
+    print("Há um problema no seu HTML.\nÉ possível que você não esteja usando o guia exegético personalizado somente com a seção 'Palavra por palavra'.\nNo Logos, crie um novo guia, exclua todas as seções, deixando somente a seção 'Palavra por palavra' mostrando todas as partes do discurso.\nExporte novamente o HTML a partir desse novo guia personalizado e tente novamente.")
+    input("Pressione Enter para encerrar...")
+    sys.exit()
 
-allps = soup.find_all('p')
+# Número de versículos
+verse_count = len(body.find_all("blockquote", recursive=False))
+print("Número de versos: ", verse_count, "\n")
 
-for p in allps:
-    style = p.get('style')
+# Encontra todos os versículos
+verse_blockquotes = []
+for bq in body.find_all('blockquote', recursive=False):
+    verse_blockquotes.append(bq)
+verse_divs = []
+for div in body.find_all('div', recursive=False):
+    style = div.get('style')
     if style is not None:
-        if "background-color:rgb(215, 215, 215); margin-top:2pt; margin-bottom:2pt;" in style:
-            verse_words.append(p.span.text)
+        if not "footer" in style:
+            verse_divs.append(div)
 
-##############################
-# Pegar todas as morfologias #
-##############################
-morphs=[]
+# Lista os textos e referências
+verse_refs = []
+verse_texts = []
+for verse in verse_divs:
+    verse_ref = verse.table.tr.td.p.a.text
+    print(verse_ref)
+    verse_refs.append(verse_ref)
+    paragraph = verse.table.find("tr").findNext("tr").td.p
+    verse_text = paragraph.text
+    verse_text = verse_text.split("|")[0].rstrip(verse_text[-4])
+    verse_text = verse_text[1:]
+    print(verse_text, "\n")
+    verse_texts.append(verse_text)
 
-for p in allps:
-    morph = ""
-    if p.a is not None:
-        href = p.a.get("href")
-        if href is not None:
-            if "https://ref.ly/logosref/morph-field" in href:
-                for span in p.find_all('span'):
-                    morph = morph + span.text + " "
-                morphs.append(morph.capitalize().rstrip(morph[-1]))
+# Palavras, morfologias, lemmas e significados
+full_table=[]
+# Iteração entre versos
+i=0
+for bq in verse_blockquotes:
+    # Iteração entre palavras do verso
+    for p in bq.find_all("p", recursive=False):
+        # Palavra
+        word = p.span.text
+        # O loop aqui é necessário por causa do hebraico
+        # No hebraico, uma mesma palavra pode conter prefixos e sufixos que também possuem lemmas, traduções e morfologias
+        last = False
+        analysis = ""
+        word_feats=[]
+        while last == False:
+            # Próximos 3 blockquotes
+            if word_feats == []:
+                word_feats = p.find_next_siblings("blockquote", limit=3)
+            else:
+                word_feats = word_feats[2].find_next_siblings("blockquote", limit=3)
+            # Lemma
+            lemma = word_feats[0].find("a").span.text
+            # Traduções
+            translation = word_feats[0].find("span", attrs={'style':'font-weight:bold;'}).text
+            # Morfologias
+            morph = ""
+            for span in word_feats[1].p.find_all('span'):
+                morph = morph + span.text + " "
+            morph = morph.capitalize().rstrip(morph[-1])
+            analysis = analysis + morph + " (" + lemma + "): " + translation + "."
+            if word_feats[2].find_next_sibling() is None:
+                last = True
+            elif word_feats[2].find_next_sibling().name != "blockquote":
+                last = True
+            else:
+                analysis = analysis + "\n"
+        # Preenche a tabela
+        full_table.append([verse_refs[i], word, analysis])
+    i += 1
 
-#########################
-# Pegar todos os lemmas #
-#########################
-lemmas=[]
+print("Foram encontradas ", len(full_table), " palavras no arquivo.\n")
 
-allas = soup.find_all('a')
-
-for a in allas:
-    href = a.get("href")
-    if href is not None:
-        if "https://ref.ly/logos4/Guide?lemma=" in href:
-            lemmas.append(a.span.text)
-
-###############################
-# Pegar todos os significados #
-###############################
-meanings=[]
-
-allspans = soup.find_all('span')
-
-for span in allspans:
-    style = span.get("style")
-    par1 = span.parent
-    par2 = par1.parent
-    if style is not None:
-        if "font-weight:bold;" in style:
-            if "p" in par1.name:
-                if "td" in par2.name:
-                    meanings.append(span.text)
-
-print("Palavras:", len(verse_words))
-print("Análises:", len(morphs))
-print("  Lemmas:", len(lemmas))
-print("Sentidos:", len(meanings))
-
-##import csv
-##
-##if(len(verse_words) == len(morphs) == len(lemmas) == len(meanings)):
-##    with open(file_path.replace(".html", ".csv"), 'w', encoding="utf-32", newline='') as file:
-##        writer = csv.writer(file, delimiter = "|")
-##        writer.writerow(["Manuscrito", "Análise morfológica", "Tradução"])
-##        for x in range(len(verse_words)):
-##            writer.writerow([verse_words[x], morphs[x] + " (" + lemmas[x] + "): " + meanings[x], ""])
-
+# Registros na planilha
 import xlsxwriter
 
-workbook = xlsxwriter.Workbook(file_path.replace(".html", ".xlsx"))
+file_path = file_path.replace(".html", ".xlsx")
+workbook = xlsxwriter.Workbook(file_path)
 worksheet = workbook.add_worksheet()
 
-worksheet.write(0, 0, "Manuscrito")
-worksheet.write(0, 1, "Análise morfológica")
-worksheet.write(0, 2, "Tradução")
+# Registro dos textos dos versículos
+for x in range(len(verse_refs)):
+    worksheet.write(x, 0, verse_refs[x])
+    worksheet.write(x, 1, verse_texts[x])
 
-for x in range(len(verse_words)):
-    worksheet.write(x+1, 0, verse_words[x])
-    worksheet.write(x+1, 1, morphs[x] + " (" + lemmas[x] + "): " + meanings[x], "")
+# Registro das palavras
+worksheet.write(len(verse_ref), 0, "Ref")
+worksheet.write(len(verse_ref), 1, "Manuscrito")
+worksheet.write(len(verse_ref), 2, "Análise morfológica")
+worksheet.write(len(verse_ref), 3, "Tradução")
+
+for x in range(len(full_table)):
+    worksheet.write(x+len(verse_ref)+1, 0, full_table[x][0])
+    worksheet.write(x+len(verse_ref)+1, 1, full_table[x][1])
+    worksheet.write(x+len(verse_ref)+1, 2, full_table[x][2])
      
 workbook.close()
 
-print("Deu bom")
+print("Planilha gerada com sucesso.\nCaminho: ", file_path, "\n\n")
+intput("Pressione Enter para encerrar...")
